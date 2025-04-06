a634beab81bfa3b9bc8b2d3c212672a54fb83881
GodOfLac <feckyouass@gmail.com>
2025-04-06 21:26:18 +0700

    tách header, footer, menu, thêm khung của cartcart

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/dangky', function () {
-    return view('dangky');
+Route::get('/cart', function () {
+    return view('cart.index'); // Giao diện giỏ hàng
 });
+
+// Route::get('/contact', function () {
+//     return view('contact'); // Giao diện liên hệ
+// });
+
+Route::get('/login', function () {
+    return view('welcome'); // Giao diện đăng nhập
+});
+// Route::get('/dangky', function () {
+//     return view('dangky');
+// });
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
+Route::post('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
+Route::post('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
